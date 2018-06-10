f693b83203b247de37dde9e0db28d619f43d1c44
Chase <chasevandiver13@gmail.com>
2018-06-10 17:09:23 -0500

    Allow user to add more than 60 mins

--- timecalc.py
+++ timecalc.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python2.7
 import sys
 
-query = sys.argv[1] #4:03+33
+query = sys.argv[1].replace('') #4:03+33
+
 time, mins = query.split('+')
 mins = int(mins) # time, mins = "4:03", 30
 time = map(int, time.split(':')) # time = [4,3]
-if mins + time[1] > 60:
+time[1] = mins + time[1]
+while time[1] > 59:
     time[0] += 1
-    time[1] = mins + time[1] - 60
-else:
-    time[1] = mins + time[1]
+    time[1] -= 60
+
 
 print "{}:{}".format(time[0], time[1] if time[1] > 9 else "0" + time[1])
